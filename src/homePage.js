c3ecd732bd1c4623aef21cfa57239e174a15050e
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-04-06 13:34:04 +0530

    Implement webpack style image assets.
    Transformed all the image assets in the project to
    use webpack's assets/resources style for images.

--- src/homePage.js
+++ src/homePage.js
@@ -1,11 +1,12 @@
 import './styles.css';
+import logo from './fryingNemo.jpg';
 
 export default function homePage(ele) {
     const subContent = document.createElement('div');
     //const subContent = document.createElement('ul');
     subContent.id = "homePageSubContent";
     subContent.classList.add('subcontent');
-    const subContentImage = document.createElement('img');
+    const subContentImage = new Image();
     const subContentHeading = document.createElement('h1');
     const subContentPara = document.createElement('p');
     /*
@@ -14,7 +15,7 @@ export default function homePage(ele) {
     const subContentHeading = document.createElement('h1');
     const subContentPara = document.createElement('p');
     */
-    subContentImage.src = '../frying-nemo-s-new-logo.jpg';
+    subContentImage.src = logo;
     subContentHeading.innerText = 'Frying Nemo!';
     subContentPara.innerText = "Welcome to Frying Nemo, where the taste of the sea meets the sizzle of the kitchen! Our restaurant offers an unforgettable culinary experience that is sure to tantalize your taste buds.\n\nAt Frying Nemo, we take great pride in serving only the freshest seafood sourced directly from the ocean. Our expert chefs use their creativity and skill to bring out the unique flavors of each dish, creating a menu that is both classic and innovative.\n\nWhether you're in the mood for a classic fish and chips or a more exotic seafood platter, our menu has something for everyone. We also offer a wide range of vegetarian and gluten-free options, ensuring that all of our guests can enjoy a delicious meal.\n\nOur restaurant has a warm and welcoming atmosphere that is perfect for a romantic dinner, a family outing, or a night out with friends. With attentive service and a menu that is sure to impress, Frying Nemo is the perfect place to celebrate any occasion.\n\nWe take great care to ensure that every guest leaves our restaurant satisfied and eager to return. Come visit us at Frying Nemo and experience the ultimate seafood dining experience!.";
     subContentPara.style.width = '80%'
